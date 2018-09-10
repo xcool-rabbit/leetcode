4d89b27673f3e3500b10ea5ff8d478b82bcf60fd
Jie Zhao <cdzj699@gmail.com>
2018-09-10 10:50:58 +0800

    2018.9.10 136

---
+++ 136.Single Number/Solution.java
@@ -0,0 +1,13 @@
+//136. 只出现一次的数字
+/*
+  利用异或的特性，相同的数异或为0，所以对数组中所有元素异或，每两个相同的元素异或为0，所以最终若有只出现一次的数字，则结果不为0
+*/
+class Solution {
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for (int i : nums) {
+            res ^= i;
+        }
+        return res;
+    }
+}
