6f370d1097721ca1f167f9e0b46e782d5ce38717
Jie Zhao <cdzj699@gmail.com>
2020-08-24 23:18:20 +0800

    2020.8.24 235 703

---
+++ 235. Lowest Common Ancestor of a Binary Search Tree/Solution.java
@@ -0,0 +1,106 @@
+//235. 二叉搜索树的最近公共祖先
+/*
+  之前做过一个普通树的最近公共祖先，现在回头看一下这种方法还挺巧妙的，自己想的
+  但是原样搬过来并不是很快，所以说二叉搜索树对这个问题有些可以利用的特性
+  执行用时：7 ms 已经战胜 39.77 % 的 java 提交记录
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    int pVal, qVal;
+    TreeNode ancestor;
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        pVal = p.val;
+        qVal = q.val;
+        dfs(root);
+        return ancestor;
+    }
+
+    private boolean dfs(TreeNode node) {
+        if (node == null) {
+            return false;
+        }
+        int times = 0;
+        boolean has = false;
+        if (isEqual(node.val)) {
+            times++;
+            has = true;
+        }
+        if (dfs(node.left)) {
+            times++;
+            has = true;
+        }
+        if (dfs(node.right)) {
+            times++;
+            has = true;
+        }
+        if (times == 2) {
+            ancestor = node;
+        }
+        return has;
+    }
+
+    private boolean isEqual(int val) {
+        return val == pVal || val == qVal;
+    }
+}
+/*
+  果然不出我所料，利用二叉搜索树的大小关系，节省了很多不必要的遍历，速度得到了提升
+  执行用时：6 ms 已经战胜 99.77 % 的 java 提交记录
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    int min, max;
+    TreeNode ancestor;
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        min = p.val < q.val ? p.val : q.val;
+        max = p.val < q.val ? q.val : p.val;
+        dfs(root);
+        return ancestor;
+    }
+
+    private boolean dfs(TreeNode node) {
+        if (node == null) {
+            return false;
+        }
+        int times = 0;
+        boolean has = false;
+        if (isEqual(node.val)) {
+            times++;
+            has = true;
+        }
+        if (node.val > min && dfs(node.left)) {
+            times++;
+            has = true;
+        }
+        if (node.val < max && dfs(node.right)) {
+            times++;
+            has = true;
+        }
+        if (times == 2) {
+            ancestor = node;
+        }
+        return has;
+    }
+
+    private boolean isEqual(int val) {
+        return val == min || val == max;
+    }
+}
---
+++ 703. Kth Largest Element in a Stream/Solution.java
@@ -0,0 +1,37 @@
+//703. 数据流中的第K大元素
+/*
+  topK问题首先想到的就是堆，这个题有很多的边界情况很烦
+  执行用时：20 ms 已经战胜 77.13 % 的 java 提交记录
+*/
+class KthLargest {
+    PriorityQueue<Integer> pq;
+    int top;
+    public KthLargest(int k, int[] nums) {
+        pq = new PriorityQueue<>(k);
+        top = k;
+        for (int n : nums) {
+            if (pq.size() < k) {
+                pq.offer(n);
+            } else if (n > pq.peek()) {
+                pq.poll();
+                pq.offer(n);
+            }
+        }
+    }
+    
+    public int add(int val) {
+        if (pq.size() < top) {
+            pq.offer(val);
+        } else if (val > pq.peek()) {
+            pq.poll();
+            pq.offer(val);
+        }
+        return pq.peek();
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
